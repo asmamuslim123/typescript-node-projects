3af990cf22c43ba3559a71261e56e2df22756461
Asma muslim <muhammadrayaan337@gmail.com>
2024-04-13 15:02:37 +0500

    first commit

---
+++ project01_number_guessing_game/index.ts
@@ -0,0 +1,27 @@
+import inquirer from 'inquirer';
+
+type ansType = {
+    userGuess: number
+}
+
+const randomNumber = Math.floor(Math.random() * 10 + 1);
+
+console.log("Welcome to number Guesing Game");
+
+const answers = await inquirer.prompt([
+    {
+        type: "number",
+        name: "userGuess",
+        message: "Please guess a number between 1 to 10: "
+    }
+]);
+
+const {userGuess} = answers;
+console.log(userGuess, "userGuess", randomNumber, 'SYs')
+if(userGuess === randomNumber){
+
+    console.log("Yeaaaa Your answer is correct \n You Win!")
+} else {
+    console.log("Please try again Better luck next time!")
+}
+  
